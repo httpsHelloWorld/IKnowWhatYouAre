6ad9aa627d07f25b8ee4a525e7c952068536ab45
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 13:17:56 -0500

    const apiRoutes = require('./API');

--- controllers/index.js
+++ controllers/index.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 const homepageRoutes = require('./homepage-routes');
 
 const dashboardRoutes = require('./dashboard-routes');
+
+const apiRoutes = require('./API');
