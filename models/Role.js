d215d366f42ec3f09e1090894a157749191cd93d
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 14:49:46 -0500

    Role.init

--- models/Role.js
+++ models/Role.js
@@ -3,3 +3,23 @@ const {Model,DataTypes} = require('sequelize');
 const sequelize = require('../configure/connection');
 
 class Role extends Model{};
+
+Role.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName:true,
+        underscored:true,
+        modelName:'role'
+    }
+);
