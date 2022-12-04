da79dc65818d1fc8609f8207b9a79623b2499866
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 14:10:48 -0500

    const roleRoutes = require('./role-routes');

--- controllers/API/index.js
+++ controllers/API/index.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 const resourceRoutes = require('./resource-routes');
 
 const userRoutes = require('./user-routes');
+
+const roleRoutes = require('./role-routes');
