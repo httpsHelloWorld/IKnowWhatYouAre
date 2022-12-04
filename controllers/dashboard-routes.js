462d3e5cf5cef608b64e2d7f9080fca87b82565e
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 14:02:05 -0500

    router.get('/',withAuth,(req,res) => {     console.log(req.expresssession.admin);     Resource.findAll(         {             attributes:['id','name','description','link']         }     )     .then(resourceDB => {         console.log(resourceDB);         const resources = resourceDB.map(resource => resource.get({plain:true}));         res.render('dashboard',{resources,admin:req.expresssession.admin})     })     .catch(err => {         console.log(err);         res.status(500).json(err)     }) });

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -6,6 +6,22 @@ const { Resource } = require('../models');
 
 const withAuth = require('../utils/auth');
 
-
+router.get('/',withAuth,(req,res) => {
+    console.log(req.expresssession.admin);
+    Resource.findAll(
+        {
+            attributes:['id','name','description','link']
+        }
+    )
+    .then(resourceDB => {
+        console.log(resourceDB);
+        const resources = resourceDB.map(resource => resource.get({plain:true}));
+        res.render('dashboard',{resources,admin:req.expresssession.admin})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+});
 
 module.exports = router;
