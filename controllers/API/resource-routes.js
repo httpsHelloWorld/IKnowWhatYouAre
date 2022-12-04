9e346ffa7ebf0a605d61d96360a92a6e4b17aad3
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 14:29:43 -0500

    const withAuth = require('../../utils/auth');

--- controllers/API/resource-routes.js
+++ controllers/API/resource-routes.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 const {Resource} = require('../../models');
 
 const {sequelize} = require('../../utils/auth');
+
+const withAuth = require('../../utils/auth');
