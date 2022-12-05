59cdfb21d787a9844ff59600356cd75006347713
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 19:50:41 -0500

    class User extends Model

--- models/User.js
+++ models/User.js
@@ -3,3 +3,9 @@ const {Model,DataTypes} = require('sequelize');
 const sequelize = require('../configure/connection');
 
 const bcrypt = require('bcrypt');
+
+class User extends Model{
+    checkPassword(loginPassword){
+        return bcrypt.compareSync(loginPassword,this.password);
+    }
+};
