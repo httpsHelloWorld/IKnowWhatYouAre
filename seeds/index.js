36ed319694ec5d7f94dfd60743905e6dadd3eb65
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 20:29:04 -0500

    const seedAll

--- seeds/index.js
+++ seeds/index.js
@@ -5,3 +5,12 @@ const roleSeeds = require('./role-seeds');
 const resourceSeeds = require('./resource-seeds');
 
 const sequelize = require('../configure/connection');
+
+const seedAll = async() => {
+    await sequelize.sync({
+        force:true
+    });
+    await userSeeds();
+    await roleSeeds();
+    await resourceSeeds();
+};
