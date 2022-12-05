b9740aeb0e124eafd699b3eaf2d20da20fb7ef29
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 20:38:51 -0500

    const roleData

--- seeds/role-seeds.js
+++ seeds/role-seeds.js
@@ -1,1 +1,16 @@
 const {Role} = require('../models');
+
+const roleData = [
+    {
+        id:1,
+        name:'admin'
+    },
+    {
+        id:2,
+        name:'miner'
+    },
+    {
+        id:3,
+        name:'buyer'
+    }
+];
