5cbcbd6d5d344b666dfbc52861c9c7bb71bb61bb
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 19:40:44 -0500

    Resource.init

--- models/Resource.js
+++ models/Resource.js
@@ -3,3 +3,32 @@ const {Model,DataTypes} = require('sequelize');
 const sequelize = require('../configure/connection');
 
 class Resource extends Model{};
+
+Resource.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        description:{
+            type:DataTypes.STRING,
+            allowNull:true
+        },
+        link:{
+            type:DataTypes.STRING,
+            allowNull:false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName:true,
+        underscored:true,
+        modelName:'resource'
+    }
+);
