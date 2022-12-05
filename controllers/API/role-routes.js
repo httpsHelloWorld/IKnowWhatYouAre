03b37624e25049cd90a34ef40ff288fc0932615f
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 23:17:47 -0500

    router.get('/:id'

--- controllers/API/role-routes.js
+++ controllers/API/role-routes.js
@@ -18,3 +18,23 @@ router.get('/',(req,res) => {
         res.status(500).json(err)
     })
 });
+
+router.get('/:id',(req,res) => {
+    Role.findOne({
+        where:{
+            id:req.params.id
+        },
+        attributes:{
+            exclude:['name']
+        },
+        include:[{
+            model:User,
+            attributes:['email']
+        }]
+    })
+    .then(roleData => res.json(roleData))
+    .catch(err => {
+        console.log(err),
+        res.status(500).json(err)
+    })
+});
