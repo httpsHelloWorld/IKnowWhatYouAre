d22dfdfe01841d0c35470b7f6be815029920f33a
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 22:47:19 -0500

    router.post('/',withAuth,roleAuth,(req,res) => {     Resource.create({         name:req.body.name,         description:req.body.description,         link:req.body.link     })     .then(resourceDB => {         res.json(resourceDB)     })     .catch(err => {         console.log(err);         res.status(400).json(err)     }) });

--- controllers/API/resource-routes.js
+++ controllers/API/resource-routes.js
@@ -16,3 +16,18 @@ router.get('/',withAuth,(req,res) => {
         res.status(500).json(err)
     })
 });
+
+router.post('/',withAuth,roleAuth,(req,res) => {
+    Resource.create({
+        name:req.body.name,
+        description:req.body.description,
+        link:req.body.link
+    })
+    .then(resourceDB => {
+        res.json(resourceDB)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err)
+    })
+});
