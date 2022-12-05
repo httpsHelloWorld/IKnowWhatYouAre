21a0f4777b896b58c6a2d29eaefcb885d1acf909
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 23:15:25 -0500

    router.get('/'

--- controllers/API/role-routes.js
+++ controllers/API/role-routes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
 
 const {Role,User} = require('../../models');
+
+router.get('/',(req,res) => {
+    Role.findAll({
+        attributes:{
+            exclude:['name']
+        },
+        include:[{
+            model:User,
+            attributes:['email']
+        }]
+    })
+    .then(roleDB => res.json(roleDB))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+});
