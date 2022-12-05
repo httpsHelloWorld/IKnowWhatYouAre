9a4d893279bf0ed10434ac8cf1a22a204fe6ce52
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 20:27:46 -0500

    const sequelize = require('../configure/connection');

--- seeds/index.js
+++ seeds/index.js
@@ -3,3 +3,5 @@ const userSeeds = require('./user-seeds');
 const roleSeeds = require('./role-seeds');
 
 const resourceSeeds = require('./resource-seeds');
+
+const sequelize = require('../configure/connection');
