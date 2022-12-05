16b25cfde4e4ef50e0821bcdcd5612c391f59920
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 20:04:05 -0500

    const roleAuth

---
+++ utils/role-auth.js
@@ -0,0 +1,8 @@
+const roleAuth = (req,res,next) => {
+    if(req.session.rold_id !== 1){
+        alert('You are not authorized to login.');
+        res.redirect('/dashboard')
+    } else{
+        next()
+    }
+};
