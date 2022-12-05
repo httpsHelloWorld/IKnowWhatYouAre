aecf49245766d8244255255aadb9c80d62ab4b3f
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 22:44:50 -0500

    router.get('/',withAuth,(req,res) => {     Resource.findAll()     .then(resourceDB => res.json(resourceDB))     .catch(err => {         console.log(err);         res.status(500).json(err)     }) });

--- controllers/API/resource-routes.js
+++ controllers/API/resource-routes.js
@@ -7,3 +7,12 @@ const {sequelize} = require('../../models/User');
 const withAuth = require('../../utils/auth');
 
 const roleAuth = require('../../utils/role-auth');
+
+router.get('/',withAuth,(req,res) => {
+    Resource.findAll()
+    .then(resourceDB => res.json(resourceDB))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+});
