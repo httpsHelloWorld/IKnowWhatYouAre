59d1ec6296f57772812eb079be36e47b328839cd
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 20:32:36 -0500

    const userData

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,1 +1,19 @@
 const {User} = require('../models');
+
+const userData = [
+    {
+        email:'userone@useronemail.come',
+        password:'passwordone',
+        role_id:1
+    },
+    {
+        email:'usertwo@usertwomail.com',
+        password:'passwordtwo',
+        role_id:2
+    },
+    {
+        email:'userthree@userthreemail.com',
+        password:'passwordthree',
+        role_id:2
+    }
+];
