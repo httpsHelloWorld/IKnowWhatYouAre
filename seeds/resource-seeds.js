98a6f16bbcf8d919e20bbb80f29f7e64acc6a3ac
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-04 20:47:56 -0500

    resourceData

--- seeds/resource-seeds.js
+++ seeds/resource-seeds.js
@@ -1,1 +1,19 @@
 const {Resource} = require('../models');
+
+const resourceData = [
+    {
+        name:'resourceOne',
+        description:'oneResource',
+        link:'minecraft.com'
+    },
+    {
+        name:'resourceTwo',
+        description:'twoResource',
+        link:'microsoft.com'
+    },
+    {
+        name:'resourceThree',
+        description:'threeResource',
+        link:'xbox.com'
+    }
+];
